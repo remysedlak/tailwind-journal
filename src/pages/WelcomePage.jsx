22e56381b95810fe968328415e6291dc3cb1de0d
remysedlak <remysedlak@gmail.com>
2025-03-22 19:25:12 -0400

    mobile welcome page height fix

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -11,15 +11,18 @@ const WelcomePage = ({ onSaveName }) => {
   };
 
   return (
-    <div className="flex flex-col items-center h-full w-full justify-around p-2 text-center">
+    <div className="flex flex-col items-center h-full w-full justify-between lg:justify-around p-2 text-center">
+      {/* Title */}
       <div className="flex lg:flex-row flex-col justify-around text-3xl lg:text-6xl">
         <h1>welcome to&nbsp;</h1>
         <div className="flex flex-row">
           <h1 className="text-purple-600 ">journal</h1><h1 className="text-blue-600">logger</h1>
         </div>
       </div>
-      <p className="lg:text-3xl/15 text-xl/15 text-gray-700 lg:w-1/2">we are happy to have you here!<br/>
+      {/* Paragraph */}
+      <p className="lg:text-3xl/15 text-xl/15 text-gray-700 lg:w-1/2 w-3/4">we are happy to have you here!<br/>
       let's get started with your first name.</p>
+      {/* name */}
       {!isSubmitted ? (
         <>
           <form onSubmit={handleSubmit} className="flex flex-col items-center">
