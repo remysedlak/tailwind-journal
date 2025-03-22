6138c575211db65267c1d5dd58c3d64362149c6a
remysedlak <remysedlak@gmail.com>
2025-03-22 19:18:55 -0400

    better welcome page for mobile

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -12,12 +12,13 @@ const WelcomePage = ({ onSaveName }) => {
 
   return (
     <div className="flex flex-col items-center h-full w-full justify-around p-2 text-center">
-      <div className="flex flex-row justify-around text-6xl">
+      <div className="flex lg:flex-row flex-col justify-around text-3xl lg:text-6xl">
         <h1>welcome to&nbsp;</h1>
-        <h1 className="text-purple-600 ">journal</h1>
-        <h1 className="text-blue-600">logger</h1>
+        <div className="flex flex-row">
+          <h1 className="text-purple-600 ">journal</h1><h1 className="text-blue-600">logger</h1>
+        </div>
       </div>
-      <p className="text-3xl/15 text-gray-700 w-1/2">we are happy to have you here!<br/>
+      <p className="lg:text-3xl/15 text-xl/15 text-gray-700 lg:w-1/2">we are happy to have you here!<br/>
       let's get started with your first name.</p>
       {!isSubmitted ? (
         <>
@@ -27,7 +28,7 @@ const WelcomePage = ({ onSaveName }) => {
               value={name}
               onChange={(e) => setName(e.target.value)}
               placeholder="enter name here :)"
-              className="text-3xl text-center pb-4"
+              className="lg:text-3xl text-xl text-center pb-4"
             />
           </form>
         </>
