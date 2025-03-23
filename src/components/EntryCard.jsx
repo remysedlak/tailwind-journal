d44fb9fec6cde6635f2922ea2b35f953138e46ba
remysedlak <remysedlak@gmail.com>
2025-03-22 22:01:42 -0400

    fix EntryCard layout for improved structure and readability

--- src/components/EntryCard.jsx
+++ src/components/EntryCard.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 function EntryCard({ title, content, date, onDelete }) {
   return (
-    <li className="bg-gray-100 flex flex-col entry-card border text-lg p-2 rounded">
-        <h2>title: {title}</h2>
+    <div>
+      <h2>title: {title}</h2>
+    
+    <li className="bg-gray-100 flex flex-col entry-card border text-lg p-2 my-2 rounded">
+        
         <p>text: {content}</p>
         <p>date: {date}</p>
     </li>
+    </div>
   );
 }
 export default EntryCard;
